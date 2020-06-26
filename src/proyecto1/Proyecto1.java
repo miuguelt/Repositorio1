77dad2d1ab57bd14e6586ce584c2a530d8228566
miuguelt <miuguelt@gmail.com>
2020-06-26 10:43:16 -0500

    Update Proyecto1.java
    Corregí un error.

--- src/proyecto1/Proyecto1.java
+++ src/proyecto1/Proyecto1.java
@@ -18,10 +18,7 @@ public class Proyecto1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        JOptionPane.showMessageDialog(null, "HOla mundo");
-        sadf
-                asdf
-                
+        JOptionPane.showMessageDialog(null, "HOla mundo");                
     }
     
 }
